22b80273cdfdf347bb89578d9436677c8c165cae
borisbrunop <borisbruno88@gmail.com>
2020-09-10 20:46:26 -0400

    backend finalizado

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Contact, Group
 #from models import Person
 
 app = Flask(__name__)
@@ -30,14 +30,223 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/contact/all', methods=['GET'])
+def r_contact():
+    # crar variable y asignarle todos los contactos
+    contacts = Contact.query.all()
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    contact_serialize = list(map(lambda contact: contact.serialize(), contacts))
+    print(contact_serialize)
+    return jsonify(contact_serialize), 200
 
-    return jsonify(response_body), 200
+@app.route('/contact/<contact_id>', methods=["GET", "PATCH", "DELETE"])
+def rud_contact_id(contact_id):
+    #crea variable y le asigna el contacto del id especificado
+    contact = Contact.query.get(contact_id)
+    #verificar si el contact existe
+    if isinstance(contact, Contact):
+        if request.method == "GET":
+            return jsonify(contact.serialize()), 200
+        elif request.method == "PATCH":
+            #crear variable con contenido del body
+            dictionary = request.json
+            if ("groups" not in dictionary):
+                #actualizar propiedades con el diccionario
+                contact.update_contact(dictionary)
+                #hacer commit
+                try:
+                    db.session.commit()
+                    #devolver el donante serializado 
+                    return jsonify(contact.serialize()), 200
+                except Exception as error:
+                    db.session.rollback()
+                    print(f"{error.args} {type(error)}")
+                    return jsonify({
+                        "result": f"{error.args}"
+                    }), 500
+            else:
+                #crea variable y le asigna el grupo con el id indicado en el body(diccionario)
+                group = Group.query.get(dictionary["groups"])
+                #group = dictionary["groups"]
+                #agrega el grupo en el contacto
+                contact.groups.append(group)
+                #hacer commit
+                try:
+                    db.session.commit()
+                    #devolver el donante serializado 
+                    return jsonify(contact.serialize()), 200
+                except Exception as error:
+                    db.session.rollback()
+                    print(f"{error.args} {type(error)}")
+                    return jsonify({
+                        "result": f"{error.args}"
+                    }), 500
+        else:
+            #remover el contact especifico 
+            db.session.delete(contact)
+            #hacer commit
+            try:
+                db.session.commit()
+                return jsonify({}), 204
+            except Exception as error:
+                db.session.rollback()
+                print(f"{error.args} {typr(error)}")
+                return jsonify({
+                    "result": f"{error.args}"
+                }), 500
+    else:
+        #el contacto no existe
+        return jsonify({
+            "result": "contact not here"
+        }), 404
+
+
+
+@app.route('/contact', methods=['POST'])
+def c_contact():
+    # crea una variable y le asigna el body enviado con .json
+    body = request.json
+
+    # verificacion de que los datos esten en orden 
+    if body is None:
+        return jsonify({
+            "result": "nothing in body"
+        }), 400
+    
+    if (
+        "full_name" not in body or
+        "email" not in body or
+        "address" not in body or
+        "phone" not in body 
+    ):
+        return jsonify({
+            "result": "check the properties"
+        }), 400
+    
+    if (
+        body["full_name"] == "" or
+        body["email"] == "" or
+        body["address"] == "" or
+        body["phone"] == "" 
+    ):
+        return jsonify({
+            "result": "check the porperties"
+        }), 400 
+    
+    # crea una varieble y se le asigna los datos validos
+    new_contact = Contact.register(
+        body["full_name"],
+        body["email"],
+        body["address"],
+        body["phone"]
+    )
+
+    # agregar a la sesion de datos sqlalchemy y hacer commit de la transacción
+    db.session.add(new_contact)
+    try:
+        db.session.commit()
+        # devolvemos el nuevo contacto serializado y 201
+        return jsonify(new_contact.serialize()), 201
+    except Exception as error:
+        db.session.rollback()
+        print(f"{error.args} {type(error)}")
+        # devolvemos el error
+        return jsonify({
+            "result": f"{error.args}"
+        }),500
+
+
+#creamos los endpoints para los GRUPOS
+@app.route('/group/all', methods=['GET'])
+def r_group():
+    # crar variable y asignarle todos los contactos
+    groups = Group.query.all()
+
+    group_serialize = list(map(lambda group: group.serialize(), groups))
+    print(group_serialize)
+    return jsonify(group_serialize), 200
+
+
+@app.route('/group', methods=['POST'])
+def c_group():
+    #crea variable y asigna body
+    body = request.json
+    #verificar contenido
+    if body is None:
+        return jsonify({
+            "result": "nothing in body"
+        }), 400
+    if (
+        "name" not in body
+    ):
+        return jsonify({
+            "result": "check the properties"
+        }), 400
+
+    if (
+        body["name"] == ""
+    ):
+        return jsonify({
+            "result": "check the properties"
+        }), 400
+    #crear grupo en sesion de base de datos si los datos estan en orden
+    new_group = Group.create_group(body["name"])
+    #agregar sesion en base da datos 
+    db.session.add(new_group)
+    try:
+        db.session.commit()
+        # devolvemos el nuevo grupo serializado y 201
+        return jsonify(new_group.serialize()), 201
+    except Exception as error:
+        db.session.rollback()
+        print(f"{error.args} {type(error)}")
+        # devolvemos el error
+        return jsonify({
+            "result": f"{error.args}"
+        }),500
+
+@app.route('/group/<group_id>', methods=['GET', 'PATCH', 'DELETE'])
+def rud_group(group_id):
+    #crea variable y le asigna un grupo con id especifico
+    group = Group.query.get(group_id)
+    if isinstance(group, Group):
+        if request.method == "GET":
+            return jsonify(group.serialize()), 200
+        elif request.method == "PATCH":
+            #crear variable con contenido del body
+            dictionary = request.json
+            #actualizar propiedades con el diccionario
+            group.update_group(dictionary)
+            #hacer commit
+            try: 
+                db.session.commit()
+                #devolver el donante serializado 
+                return jsonify(group.serialize()), 200
+            except Exception as error:
+                db.session.rollback()
+                print(f"{error.args} {type(error)}")
+                return jsonify({
+                    "result": f"{error.args}"
+                }), 500
+        else:
+            #remover el contact especifico 
+            db.session.delete(group)
+            #hacer commit
+            try:
+                db.session.commit()
+                return jsonify({}), 204
+            except Exception as error:
+                db.session.rollback()
+                print(f"{error.args} {typr(error)}")
+                return jsonify({
+                    "result": f"{error.args}"
+                }), 500
+    else:
+        #el contacto no existe
+        return jsonify({
+            "result": "contact not here"
+        }), 404
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -17,3 +17,97 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(30), nullable=False)
+    email = db.Column(db.String(30), nullable=False)
+    address = db.Column(db.String(60), nullable=False)
+    phone = db.Column(db.Integer, nullable=False)
+    groups = db.relationship('Group', secondary="links", lazy='subquery',
+        backref=db.backref('contacts', lazy=True))
+
+    def __init__(self, full_name, email, address, phone):
+        """ crea y devuelve una instancia de esta clase """
+        self.full_name = full_name
+        self.email = email
+        self.address = address
+        self.phone = phone
+    
+    @classmethod
+    def register(cls, full_name, email, address, phone):
+        """ normaliza insumos, crea un objeto de la clase Contact
+            con esos insumos y devuelve la instancia creada
+        """
+        new_contact = cls(
+            full_name,
+            email,
+            address,
+            phone
+        )
+        return new_contact
+
+    def serialize(self):
+        """ devuelve un diccionario del objeto """
+        groups_list = self.groups
+        list_id = []
+        for group in groups_list:
+            list_id.append(group.name)
+        return {
+            "id": self.id,
+            "full_name": self.full_name,
+            "email": self.email,
+            "address": self.address,
+            "phone": self.phone,
+            "groups": list_id
+        }
+    
+    def update_contact(self, dictionary):
+        """ actualiza propiedades del donante segun 
+            contenido del diccionario 
+        """
+        for (key, value) in dictionary.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
+        return True
+
+#tabla que relaciona muchos con muchos a contact y group 
+link = db.Table('links',
+    db.Column('group_id', db.Integer, db.ForeignKey('group.id'), primary_key=True),
+    db.Column('contact_id', db.Integer, db.ForeignKey('contact.id'), primary_key=True)
+)
+
+class Group(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+
+    def __init__(self, name):
+        self.name = name
+
+    @classmethod
+    def create_group(cls, name):
+        new_group = cls(
+            name
+        )
+        return new_group
+
+    def serialize(self):
+        contacts_list = self.contacts
+        list_id = []
+        for contact in contacts_list:
+            list_id.append(contact.full_name)
+        return{
+        "id": self.id,
+        "name": self.name,
+        "contacts": list_id
+        }
+    
+    def update_group(self, dictionary):
+        """ actualiza propiedades del donante segun 
+            contenido del diccionario 
+        """
+        for (key, value) in dictionary.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
+        return True
+
